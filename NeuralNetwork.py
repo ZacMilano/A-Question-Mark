c0567e2efa0d35bf139b6b84b62e9982af9107c6
Zac Monroe <zmonroe0820@gmail.com>
2018-11-20 13:43:41 -0500

    Set data to be np.array type

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,17 +1,34 @@
 import AbeTF as tf
+import numpy as np
 from Data import *
 from math import exp
 from time import time
 
 def sigmoid(x, derivative=False):
+  '''
+  Sigmoid activation function.
+  Takes in any real number, outputs a number between 0 and 1.
+  '''
   if not derivative:
     return 1 / (1  + exp(-x))
   else:
-    return sigmoid(x, derivative=False) * (1 - sigmoid(x, derivative=False))
+    s = sigmoid(x, derivative=False)
+    return s * (1 - s)
 
 class NeuralNetwork:
-  def __init__(self):
-    self.train, self.test = Data.train_and_pseudo_test()
+  def __init__(self, data_directory='../data_samples', final_testing=False):
+    if final_testing:
+      self.train = Data(data_directory=data_directory, is_test_data=False)
+      self.test = Data(data_directory=data_directory, is_test_data=True)
+    else:
+      self.train, self.test = Data.train_and_pseudo_test()
+
+    self.train_labels = np.array(self.train.labels())
+    self.train_images = np.array(self.train.images())
+
+    self.test_labels = np.array(self.test.labels())
+    self.test_images = np.array(self.test.images())
+
 
 if __name__ == "__main__":
   time_initial = time()
