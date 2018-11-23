a2b07a8eb6ba3f0f686b38968b46be98f7b06503
Zac Monroe <zmonroe0820@gmail.com>
2018-11-23 10:38:45 -0500

    Created CNN.py (will eventually be a convolutional neural network)

---
+++ CNN.py
@@ -0,0 +1,19 @@
+import numpy as np
+from Data import *
+from time import time
+from helpers import sigmoid, softmax, cross_entropy, normalize
+from NeuralNetwork import NeuralNetwork
+
+class CNN(NeuralNetwork):
+  pass
+
+if __name__ == '__main__':
+  t0 = time()
+
+  n = CNN()
+
+  dt = time() - t0
+  mins, secs = int(dt // 60), int(dt % 60)
+  print('-'*80 +
+        '\nCompleted in {:0>2d}:{:0>2d}\n'.format(mins, secs) +
+        '-'*80)
