d65646329005748b7e3f644452bc89ba6d1c50ba
Zac Monroe <zmonroe0820@gmail.com>
2018-11-29 18:32:16 -0500

    Reorganized import statements

--- CNN.py
+++ CNN.py
@@ -1,8 +1,7 @@
 import AbeTF as tf
-import numpy as np
 from Data import *
 from time import time
-from helpers import sigmoid, softmax, cross_entropy, normalize
+from helpers import normalize_v2
 
 class CNN(tf.Model):
   name = 'cnn'
@@ -10,7 +9,8 @@ class CNN(tf.Model):
   def __init__(self, training=True):
     super().__init__() # :)
     if training:
-      self.train_data, self.test_data = Data.train_and_pseudo_test(proportion=0.9)
+      self.train_data, self.test_data = Data.train_and_pseudo_test(
+        proportion=0.9)
     else:
       self.train_data = Data(data_directory=data_directory, is_test_data=False)
       self.test_data = Data(data_directory=data_directory, is_test_data=True)
